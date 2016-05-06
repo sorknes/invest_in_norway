32a42dbd3228b4c86febf35172af904a8bc8813d
Knut Sorknes <knut.sorknes@makingwaves.com>
2016-05-06 13:39:28 +0200

    finished designing the index

--- app/js/main.js
+++ app/js/main.js
@@ -1,57 +1,61 @@
+"use strict";
+
+$( document ).ready(function() {
 // Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $("header").outerHeight();
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $("header").outerHeight();
 
-$(window).scroll(function(event) {
-    didScroll = true;
-});
+    $(window).scroll(function(event) {
+        didScroll = true;
+    });
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 300);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-
-    // Make sure they scroll more than delta
-    if (Math.abs(lastScrollTop - st) <= delta)
-        return;
-
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight) {
-        // Scroll Down
-        $("header").removeClass("nav-down").addClass("nav-up");
-    } else {
-        // Scroll Up
-        if (st + $(window).height() < $(document).height()) {
-            $("header").removeClass("nav-up").addClass("nav-down");
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
         }
+    }, 300);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        // Make sure they scroll more than delta
+        if (Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight) {
+            // Scroll Down
+            $("header").removeClass("nav-down").addClass("nav-up");
+        } else {
+            // Scroll Up
+            if (st + $(window).height() < $(document).height()) {
+                $("header").removeClass("nav-up").addClass("nav-down");
+            }
+        }
+
+        lastScrollTop = st;
     }
 
-    lastScrollTop = st;
-}
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $($(this).attr('href'));
 
-$('a[href^="#"]').on('click', function(event) {
-    var target = $($(this).attr('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
 
-    if (target.length) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
-});
-
-$('.grid').isotope({
-    itemSelector: '.grid-item',
-    percentPosition: true,
-    masonry: {
-        columnWidth: '.grid-sizer'
-    }
+    $('.grid').isotope({
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        masonry: {
+            columnWidth: '.grid-sizer'
+        }
+    });
 });
